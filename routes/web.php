3e445dbde382ae415e85d8eba0223dfd3d519380
EuclidesKinto <euclideskinto@gmail.com>
2021-01-06 14:51:45 -0300

    add autenticação

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('', ShowTweets::class);
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
